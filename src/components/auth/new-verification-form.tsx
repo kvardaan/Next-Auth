20afa98fefb537de27a232d4675969c0c79c5704
kvardaan <vardaankhattar8907@gmail.com>
2024-08-06 22:12:21 +0530

    Feature: Email Verification

---
+++ src/components/auth/new-verification-form.tsx
@@ -0,0 +1,54 @@
+'use client'
+import { BeatLoader } from 'react-spinners'
+import { useSearchParams } from 'next/navigation'
+import { useCallback, useEffect, useState } from 'react'
+
+import { FormError } from '@/components/form-error'
+import { FormSuccess } from '@/components/form-success'
+import { CardWrapper } from '@/components/auth/card-wrapper'
+import { newVerification } from '@/actions/new-verification'
+
+const NewVerificationForm = () => {
+	const [error, setError] = useState<string | undefined>()
+	const [success, setSuccess] = useState<string | undefined>()
+
+	const searchParams = useSearchParams()
+	const token = searchParams.get('token')
+
+	const onSubmit = useCallback(async () => {
+		if (success || error) return
+
+		if (!token) {
+			setError('Missing token')
+			return
+		}
+		try {
+			const data = await newVerification(token)
+			console.log(data)
+			setSuccess(data.success)
+			setError(data.error)
+		} catch {
+			setError('Something went wrong!')
+		}
+	}, [token, success, error])
+
+	useEffect(() => {
+		onSubmit()
+	}, [onSubmit])
+
+	return (
+		<CardWrapper
+			headerLabel="Confirming your verification"
+			backButtonLabel="Back to login"
+			backButtonHref="/auth/login"
+		>
+			<div className="flex items-center w-full justify-center">
+				{!success && !error && <BeatLoader color="#7f7f7f" />}
+				{!success && <FormError message={error} />}
+				<FormSuccess message={success} />
+			</div>
+		</CardWrapper>
+	)
+}
+
+export default NewVerificationForm
--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@
  * @type {string[]}
  */
 
-export const publicRoutes: string[] = ["/"];
+export const publicRoutes: string[] = ['/', '/auth/new-verification']
 
 /**
  * An array of routes that are used for authentication
@@ -12,21 +12,17 @@ export const publicRoutes: string[] = ["/"];
  * @type {string[]}
  */
 
-export const authRoutes: string[] = [
-  "/auth/login",
-  "/auth/register",
-  "/auth/error",
-];
+export const authRoutes: string[] = ['/auth/login', '/auth/register', '/auth/error']
 
 /**
  * The prefix for API authentication routes
  * Routes that start with this prefix are used for API authentication purposes
  * @type {string}
  */
-export const apiAuthPrefix: string = "/api/auth";
+export const apiAuthPrefix: string = '/api/auth'
 
 /**
  * The default redirect path after logging in
  * @type {string}
  */
-export const DEFAULT_LOGIN_REDIRECT: string = "/settings";
+export const DEFAULT_LOGIN_REDIRECT: string = '/settings'
